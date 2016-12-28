5fd01116617cf67f5a1d4f0739ee4bb2a1c9da78
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-27 23:12:09 -0500

    This is where GUI will be displayed

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class TicTacToe {
 
-
-
+	public static void main(String[]args){
+		Window g = new Window();
+		g.setVisible(true);
+	}
 }
