cbca0190cca57317b9b8bc3140e4606cbdf8704b
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2016-12-28 12:22:42 -0500

    Some gui elements added

--- BoardGUI.java
+++ BoardGUI.java
@@ -1,5 +1,16 @@
-public class BoardGUI {
+public class BoardGUI extends JFrame implements ActionListener {
 
+    private Container pane;
+    private JLabel player1, player2, wins1, wins2;
+    private JButton[][] board;
 
+    public BoardGUI () {
+
+	this.setTitle("Tic-Tac-Toe");
+	this.setSize(300, 300);
+	this.setDefaultOperation(EXIT_ON_CLOSE);
+
+	
+    
 
 }
