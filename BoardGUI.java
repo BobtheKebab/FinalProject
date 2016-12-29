ae76e22882ca5c1eee6d297064929f4d342671a4
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2016-12-28 21:48:14 -0500

    Made the container actually display panels

--- BoardGUI.java
+++ BoardGUI.java
@@ -5,28 +5,35 @@ import java.util.*;
 
 public class BoardGUI extends JFrame {
     
-    private JPanel top, bottom, mainPanel;
+    private Container pane;
+    private JPanel top, bottom;
     private JLabel player1, player2, wins1, wins2;
     private JButton[][] board;
 
     public BoardGUI () {
 
-	mainPanel = new JPanel(new BoxLayout());
-	mainPanel.setSize(300, 300);
-	mainPanel.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Tic-Tac-Toe");
+	this.setSize(300, 300);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
 
 	player1 = new JLabel("Player 1", JLabel.CENTER);
 	player2 = new JLabel("Player 2", JLabel.CENTER);
 	wins1 = new JLabel("0 Wins", JLabel.CENTER);
 	wins2 = new JLabel("0 Wins", JLabel.CENTER);
 
-	top = new JPanel(new FlowLayout());
-	top.setPreferredSize(new Dimension(100,100));
-	bottom = new JPanel(new FlowLayout());
+	top = new JPanel(new GridLayout());
+	bottom = new JPanel(new GridLayout(2, 2));
 
-	//top.add(player1);
+	bottom.add(player1);
+	bottom.add(wins1);
+	bottom.add(player2);
+	bottom.add(wins2);
 
-	mainPanel.add(top);
+	pane.add(top);
+	pane.add(bottom);
     }
 
 	
