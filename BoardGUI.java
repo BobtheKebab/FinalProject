b19706e718d5ff76ac981584556cc18462a79eff
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2016-12-28 21:26:45 -0500

    Did work on gui, still alot left :(

--- BoardGUI.java
+++ BoardGUI.java
@@ -1,16 +1,42 @@
-public class BoardGUI extends JFrame implements ActionListener {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
-    private Container pane;
+public class BoardGUI extends JFrame {
+    
+    private JPanel top, bottom, mainPanel;
     private JLabel player1, player2, wins1, wins2;
     private JButton[][] board;
 
     public BoardGUI () {
 
-	this.setTitle("Tic-Tac-Toe");
-	this.setSize(300, 300);
-	this.setDefaultOperation(EXIT_ON_CLOSE);
+	mainPanel = new JPanel(new BoxLayout());
+	mainPanel.setSize(300, 300);
+	mainPanel.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	player1 = new JLabel("Player 1", JLabel.CENTER);
+	player2 = new JLabel("Player 2", JLabel.CENTER);
+	wins1 = new JLabel("0 Wins", JLabel.CENTER);
+	wins2 = new JLabel("0 Wins", JLabel.CENTER);
+
+	top = new JPanel(new FlowLayout());
+	top.setPreferredSize(new Dimension(100,100));
+	bottom = new JPanel(new FlowLayout());
+
+	//top.add(player1);
+
+	mainPanel.add(top);
+    }
 
 	
-    
+
+	
+    public static void main (String[] args) {
+
+	BoardGUI dank = new BoardGUI();
+	dank.setVisible(true);
+
+    }
 
 }
